2f294ca31f445089d58dd3321f2ff34aa29e0512
sejongPark <lifedesigner88@gmail.com>
2024-01-04 20:34:55 +0900

    Refactor quiz solution evaluation function in Solution class

--- src/Print.java
+++ src/Print.java
@@ -1,24 +1,21 @@
 class Print {
-    static void P() {System.out.print("\n");}
-    static void P(boolean x) {System.out.print(x);}
-    static void P(char x) {System.out.print(x);}
-    static void P(int x) {System.out.print(x);}
-    static void P(long x) {System.out.print(x);}
-    static void P(float x) {System.out.print(x);}
-    static void P(double x) {System.out.print(x);}
-    static void P(char[] x) {System.out.print(x);}
-    static void P(String x) {System.out.print(x);}
-    static void P(Object x) {System.out.print(x);}
+    static void P(Object x) {
+        System.out.println(x);
+    }
 
-    static void print() {System.out.println();}
-    static void print(boolean x) {System.out.println(x);}
-    static void print(char x) {System.out.println(x);}
-    static void print(int x) {System.out.println(x);}
-    static void print(long x) {System.out.println(x);}
-    static void print(float x) {System.out.println(x);}
-    static void print(double x) {System.out.println(x);}
-    static void print(char[] x) {System.out.println(x);}
-    static void print(String x) {System.out.println(x);}
-    static void print(Object x) {System.out.println(x);}
+    static void P(char[] x) {
+        System.out.println(new String(x));
+    }
 
+    static void print(Object x) {
+        System.out.println(x);
+    }
+
+    static void print() {
+        System.out.println();
+    }
+
+    static void print(char[] x) {
+        System.out.println(new String(x));
+    }
 }
