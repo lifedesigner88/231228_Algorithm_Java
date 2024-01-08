1e815cb5d8c1a5f79aaa6879fda940a10790ef52
lifedesigner88@gmail.com <lifedesigner88@gmail.com>
2024-01-08 16:05:32 +0900

    Implement depth-first and breadth-first search algorithms
    The main part of the Programmers class has been significantly modified to add two graph traversal functions: Depth-first Search (DFS) and Breadth-first Search (BFS). These two algorithms are now implemented as separate classes (DFS and BFS), each with methods to add edges and perform the corresponding traversal. Furthermore, an integer array and an ArrayList have also been added and used as part of the testing for these search algorithms.

--- src/Programmers.java
+++ src/Programmers.java
@@ -1,17 +1,56 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public class Programmers extends Print{
     public static void main(String[] args) {
 
-        String[] quiz = {"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"};
 
-        Solution a = new Solution();
-        print(Arrays.toString(a.solution(quiz)));
 
+        int[] arr[] = new int[2][5];
+
+        print(Arrays.deepToString(arr));
+
+
+
+        DFS g = new DFS(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Depth First Traversal "+
+                "(starting from vertex 2)");
+
+        g.DFS(2);
+
+        BFS g2 = new BFS(4);
+
+        g2.addEdge(0, 1);
+        g2.addEdge(0, 2);
+        g2.addEdge(1, 2);
+        g2.addEdge(2, 0);
+        g2.addEdge(2, 3);
+        g2.addEdge(3, 3);
+
+        System.out.println("Breadth First Traversal (starting from vertex 2)");
+
+        g2.BFS(2);
+
+
+        ArrayList[] arr2 = new ArrayList[3];
+
+        arr2[0] = new ArrayList<Integer>();
+        arr2[0].add(1);
 
     }
 
 }
+
 class Solution {
     public String[] solution(String[] quiz) {
         int length = quiz.length;
@@ -31,5 +70,84 @@ class Solution {
         }
 
         return answer;
+    }
+}
+
+
+class DFS {
+    private int V; // No. of vertices
+    private final LinkedList<Integer>[] adj; //Adjacency Lists
+
+    // Constructor
+    DFS(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    // Function to add an edge into the graph
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    // Function used by DFS
+    void DFSUtil(int v, boolean visited[]) {
+        visited[v] = true;
+        System.out.print(v+" ");
+
+        Iterator<Integer> i = adj[v].listIterator();
+        while (i.hasNext()) {
+            int n = i.next();
+            if (!visited[n])
+                DFSUtil(n, visited);
+        }
+    }
+
+    // The function to do DFS traversal. It uses recursive DFSUtil()
+    void DFS(int v) {
+        boolean visited[] = new boolean[V];
+        DFSUtil(v, visited);
+    }
+
+}
+
+class BFS {
+    private int V;
+    private LinkedList<Integer> adj[];
+
+    BFS(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void BFS(int s) {
+        boolean visited[] = new boolean[V];
+
+        LinkedList<Integer> queue = new LinkedList<>();
+
+        visited[s]=true;
+        queue.add(s);
+
+        while (queue.size() != 0) {
+            s = queue.poll();
+            System.out.print(s+" ");
+
+            Iterator<Integer> i = adj[s].listIterator();
+            while (i.hasNext()) {
+                int n = i.next();
+                if (!visited[n]) {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
     }
 }
