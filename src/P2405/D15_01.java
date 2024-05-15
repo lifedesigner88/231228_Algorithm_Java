75e15ba0a733322707223375d282e43116f6faa8
SejongPark <lifedesigner88@gmail.com>
2024-05-15 12:35:18 +0900

    코테 초기 세팅

--- src/P2405/D15_01.java
+++ src/P2405/D15_01.java
@@ -1,12 +1,72 @@
 package P2405;
 
+import java.util.Arrays;
+
 public class D15_01 {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution("HI"));
+        String[][] datas = {
+                {"3141592", "271"},
+                {"500220839878", "7"},
+                {"10203", "15"},
+        };
+        System.out.println(Arrays.deepToString(datas));
+        for (String[] data : datas) {
+            System.out.println(new Solution().solution(data[0], data[1]));
+            System.out.println(new Solution2().solution(data[0], data[1]));
+        }
     }
 }
+
+// 내가 푼 코드.
 class Solution {
-    public String solution(String operations) {
-        return operations;
+    public int solution(String t, String p) {
+        int answer = 0;
+        int tlength = t.length();
+        int plength = p.length();
+        char pFirstNum = p.charAt(0);
+        long numberP = Long.parseLong(p);
+
+        for (int i = 0; i <= tlength - plength; i++) {
+            char tFirstNum = t.charAt(i);
+            if (tFirstNum < pFirstNum)
+                answer++;
+            else {
+                String subStrT = t.substring(i, i + plength);
+                long numberT = Long.parseLong(subStrT);
+                if (numberT <= numberP)
+                    answer++;
+            }
+        }
+        return answer;
+    }
+}
+
+// AI 의 리펙토링 코드
+class Solution2 {
+
+    public int solution(String t, String p) {
+
+        int count = 0;
+        int tLength = t.length();
+        int pLength = p.length();
+        char pFirstDigit = p.charAt(0);
+        long numberP = Long.parseLong(p);
+
+        for (int i = 0; i <= tLength - pLength; i++){
+            count += checkSubstrings(t, i, pLength, pFirstDigit, numberP);
+        }
+
+        return count;
+    }
+
+    private int checkSubstrings(String t, int index, int length, char firstDigit, long number) {
+        char tFirstDigit = t.charAt(index);
+
+        if(tFirstDigit < firstDigit) return 1;
+
+        String substring = t.substring(index, index + length);
+        long numberT = Long.parseLong(substring);
+
+        return numberT <= number ? 1 : 0;
     }
 }
