ccdd0c4f9f558549cbd540fcd3e430c43b0b20a7
SejongPark <lifedesigner88@gmail.com>
2024-05-15 11:14:29 +0900

    코테 초기 세팅

---
+++ src/P2405/D15_01.java
@@ -0,0 +1,12 @@
+package P2405;
+
+public class D15_01 {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution("HI"));
+    }
+}
+class Solution {
+    public String solution(String operations) {
+        return operations;
+    }
+}
