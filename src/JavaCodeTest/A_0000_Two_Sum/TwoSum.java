7f00788da642fda3990e3c4e57e1e5893cc664b9
sejongPark <lifedesigner88@gmail.com>
2024-05-22 14:34:55 +0900

    TwoSum

--- src/JavaCodeTest/A_0000_Two_Sum/TwoSum.java
+++ src/JavaCodeTest/A_0000_Two_Sum/TwoSum.java
@@ -1,36 +1,47 @@
-package JavaCodeTest.A_0001_Two_Sum;
+package JavaCodeTest.A_0000_Two_Sum;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+// https://leetcode.com/problems/two-sum
 public class TwoSum {
     public static void main(String[] args) {
 
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
 
+        BrusthForce bf = new BrusthForce();
+        System.out.println(Arrays.toString(bf.twoSum(nums, target)));
+
+        OnePassHashMap op = new OnePassHashMap();
+        System.out.println(Arrays.toString(op.twoSum(nums, target)));
+
+        TwoPassHashMap tp = new TwoPassHashMap();
+        System.out.println(Arrays.toString(tp.twoSum(nums, target)));
 
     }
 }
 
-
 class BrusthForce {
     public int[] twoSum(int[] nums, int target) {
-        int nlength = nums.length;
-        for ( int i = 0; i < nlength; i++ )
-            for ( int j = i + 1; j < nlength; j++ )
-                if ( nums[i] + nums[j] == target )
-                    return new int[]{i , j};
+        int n = nums.length;
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++)
+                if (nums[i] + nums[j] == target)
+                    return new int[]{i, j};
         return null;
     }
 }
 
 class TwoPassHashMap {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
         for (int i = 0; i < n; i++) map.put(nums[i], i);
         for (int i = 0; i < n; i++) {
             int subValue = target - nums[i];
-            if (map.containsKey(subValue) && map.get(subValue) !=i )
+            if (map.containsKey(subValue) && map.get(subValue) != i)
                 return new int[]{i, map.get(subValue)};
         }
         return null;
@@ -41,7 +52,6 @@ class OnePassHashMap {
     public int[] twoSum(int[] nums, int target) {
         Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
-
         for (int i = 0; i < n; i++) {
             int subValue = target - nums[i];
             if (map.containsKey(subValue))
