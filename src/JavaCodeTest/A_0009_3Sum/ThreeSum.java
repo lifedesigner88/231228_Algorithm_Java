4d09baed4c08fcb39a526bea99057ed8af5fb504
sejongPark <lifedesigner88@gmail.com>
2024-06-01 19:53:07 +0900

    3개의 합

--- src/JavaCodeTest/A_0009_3Sum/ThreeSum.java
+++ src/JavaCodeTest/A_0009_3Sum/ThreeSum.java
@@ -26,6 +26,7 @@ class Solution {
 
         Arrays.sort(nums);
         List<List<Integer>> results = new ArrayList<>();
+
         int n = nums.length;
         for (int i = 0; i < n - 2; i++) {
             if (i > 0 && nums[i - 1] == nums[i]) continue;
