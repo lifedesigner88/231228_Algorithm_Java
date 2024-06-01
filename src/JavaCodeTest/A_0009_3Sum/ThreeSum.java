b510e604e042006048e3e9ec6e551179b75f298f
sejongPark <lifedesigner88@gmail.com>
2024-06-01 19:52:48 +0900

    3개의 합

---
+++ src/JavaCodeTest/A_0009_3Sum/ThreeSum.java
@@ -0,0 +1,78 @@
+package JavaCodeTest.A_0009_3Sum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        int[][] result = {{-1, -1, 2}, {-1, 0, 1}};
+
+        BrustForce bf = new BrustForce();
+        List<List<Integer>> answer = bf.threeSum(nums);
+        for (List<Integer> list : answer) System.out.print(list + ", ");
+
+    }
+}
+
+
+// ❤️ Beautiful Solution ❤️
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+        List<List<Integer>> results = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i - 1] == nums[i]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum < 0) left++;
+                else if (sum > 0) right--;
+                else {
+                    results.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return results;
+    }
+}
+
+
+// 시간초과
+class BrustForce {
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+
+        List<List<Integer>> results = new LinkedList<>();
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            for (int j = i + 1; j < n - 1; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+                for (int k = j + 1; k < n; k++){
+                    if (k > j + 1 && nums[k] == nums[k - 1]) continue;
+                    if (nums[i] + nums[j] + nums[k] == 0)
+                        results.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                }
+            }
+        }
+        return results;
+    }
+}
+
+
+
