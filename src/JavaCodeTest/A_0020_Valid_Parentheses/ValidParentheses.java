d5bf6246920d2057bd9a4b7a4a7b3c5ad7b49cd3
SejongPark <lifedesigner88@gmail.com>
2024-06-05 12:04:06 +0900

    ValidParentheses

--- src/JavaCodeTest/A_0020_Valid_Parentheses/ValidParentheses.java
+++ src/JavaCodeTest/A_0020_Valid_Parentheses/ValidParentheses.java
@@ -24,20 +24,21 @@ public class ValidParentheses {
 
 class Solution {
     public boolean isValid(String s) {
-
         Deque<Character> stack = new ArrayDeque<>();
         Map<Character, Character> table = new HashMap<>() {{
-            put(')', '(');
             put('}', '{');
             put(']', '[');
+            put(')', '(');
         }};
 
         int n = s.length();
-        for (int i = 0; i < n; i++)
-            if (!table.containsKey(s.charAt(i)))
-                stack.push(s.charAt(i));
-            else if (stack.isEmpty() || table.get(s.charAt(i)) != stack.pop())
+        for (int i = 0; i < n; i++) {
+            char temp = s.charAt(i);
+            if (!table.containsKey(temp))
+                stack.push(temp);
+            else if (stack.isEmpty() || stack.pop() != table.get(temp))
                 return false;
+        }
 
         return stack.isEmpty();
     }
