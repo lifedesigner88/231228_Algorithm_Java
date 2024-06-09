6c1800f290f2d90b2ebcee5bdff628f99c8b44c3
sejongPark <lifedesigner88@gmail.com>
2024-06-09 14:12:22 +0900

    jewlsAndStones

--- src/JavaCodeTest/A_0031_Jewels_and_Stones/JewelsAndStones.java
+++ src/JavaCodeTest/A_0031_Jewels_and_Stones/JewelsAndStones.java
@@ -27,7 +27,6 @@ class UseHashMap {
     public int numJewelsInStones(String jewels, String stones) {
 
         Map<Character, Integer> map = new HashMap<>();
-
         for (char s : stones.toCharArray())
             map.put(s, map.getOrDefault(s, 0) + 1);
 
@@ -48,7 +47,7 @@ class UseHashSet {
         for (char j : jewels.toCharArray()) set.add(j);
         for (char s : stones.toCharArray())
             if (set.contains(s)) count++;
-        return count;
 
+        return count;
     }
 }
