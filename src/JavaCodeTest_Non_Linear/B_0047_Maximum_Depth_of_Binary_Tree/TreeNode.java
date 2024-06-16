dc6427878e5be89ee02e4fa1f0ecb4e5c70c8e0b
sejongPark <lifedesigner88@gmail.com>
2024-06-16 10:28:46 +0900

    MaximumDepthOfBinaryTree

---
+++ src/JavaCodeTest_Non_Linear/B_0047_Maximum_Depth_of_Binary_Tree/TreeNode.java
@@ -0,0 +1,23 @@
+package JavaCodeTest_Non_Linear.B_0047_Maximum_Depth_of_Binary_Tree;
+
+public class TreeNode {
+
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(){}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+
+}
+
+
