be89a12327e06c67aca2fb71d86bc4c631512acd
SejongPark <lifedesigner88@gmail.com>
2024-12-02 17:50:18 +0900

    https://school.programmers.co.kr/learn/courses/30/lessons/133502

--- src/Programers/Lv1/D2411/P008_햄버거만들기.java
+++ src/Programers/Lv1/D2411/P008_햄버거만들기.java
@@ -1,10 +1,12 @@
 package Programers.Lv1.D2411;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 
-// 세종 풀이법 - 시간초과
+// https://school.programmers.co.kr/learn/courses/30/lessons/133502
 public class P008_햄버거만들기 {
 
     private final int[] bugger = {1, 2, 3, 1};
@@ -45,3 +47,45 @@ public class P008_햄버거만들기 {
 // 2 야채
 // 3 고기
 // 1 빵
+
+
+class HamburgerPacking {
+
+    public static int solution(int[] ingredient) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        int count = 0;
+
+        for (int i : ingredient) {
+            deque.addLast(i);
+
+            // Check if the last 4 elements form a complete hamburger
+            if (deque.size() >= 4) {
+                // Get the last four elements
+                Integer[] lastFour = new Integer[4];
+                Object[] dequeArray = deque.toArray();
+
+                for (int j = 0; j < 4; j++) {
+                    lastFour[j] = (Integer) dequeArray[dequeArray.length - 4 + j];
+                }
+
+                if (lastFour[0] == 1 && lastFour[1] == 2 &&
+                        lastFour[2] == 3 && lastFour[3] == 1) {
+
+                    // Remove 4 items representing a complete hamburger
+                    deque.pollLast();
+                    deque.pollLast();
+                    deque.pollLast();
+                    deque.pollLast();
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int[] ingredient = {2, 1, 1, 2, 3, 1, 2, 3, 1};
+        System.out.println(solution(ingredient));  // Output: 2
+    }
+}
